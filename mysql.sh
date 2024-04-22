fe8313df5cf7f9d28f9555843acaf69875641bb5
RSireesha <ramireddysireesha20@gmail.com>
2024-04-22 10:21:41 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -10,30 +10,31 @@ Y="\e[33m"
 N="\e[0m"
 
 VALIDATE(){
- if [ $1 -ne 0 ]
- then
-    echo -e "$2... $R FAILURE $N"
-    exit 1
-else 
-    echo -e "$2... $G SUCCESS $N"
-fi
-}                          
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
 
 if [ $USERID -ne 0 ]
-then 
+then
     echo "Please run this script with root access."
-    exit 1 # manually exist if error comes.
+    exit 1 # manually exit if error comes.
 else
-   echo "You are a super user."
+    echo "You are super user."
 fi
+
 
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Server"
 
-systemctl enable mysqld -y &>>$LOGFILE
+systemctl enable mysqld &>>$LOGFILE
 VALIDATE $? "Enabling MySQL Server"
 
-systemctl start mysqld -y &>>$LOGFILE
+systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting MySQL Server"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
